e44f79d1d8751a0772cdedc9285d69504a3d289d
Alexis Herrera <alexysaguilar@gmail.com>
2025-03-15 18:14:48 -0300

    Only executes on server running

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-result=$(docker compose -f docker-compose-dev.yaml run --rm netcat-test 2>&1 | tail -n 1)
-echo "$result"
+COMPOSE_FILE="docker-compose-dev.yaml"
+
+if [ -n "$(docker compose -f $COMPOSE_FILE ps -q)" ]; then
+  result=$(docker compose -f $COMPOSE_FILE run --rm netcat-test 2>&1 | tail -n 1)
+  echo "$result"
+fi
