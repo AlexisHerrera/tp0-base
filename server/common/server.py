fa609e5f5b3f98fdb46669b15923ca38e5fdbe47
Alexis Herrera <alexysaguilar@gmail.com>
2025-03-16 12:57:38 -0300

    Handles specific error accept in socket closed

--- server/common/server.py
+++ server/common/server.py
@@ -19,7 +19,7 @@ class Server:
             # This may leave client connections alive, so closing those file descriptors is needed
             self._server_socket.close()
             logging.info("action: socket_close | result: success")
-        except Exception as e:
+        except OSError as e:
             logging.error("action: socket close | result: fail | error: {e}", e)
 
     def run(self):
@@ -34,8 +34,13 @@ class Server:
             try:
                 client_sock = self.__accept_new_connection()
                 self.__handle_client_connection(client_sock)
-            except Exception as e:
-                logging.info('action: loop stopped', e)
+            except OSError as e:
+                if not self._alive:
+                    logging.info("action: loop stopped | result: success | reason: socket closed gracefully")
+                    break
+                else:
+                    logging.error("action: loop stopped | result: fail | error: %s", e)
+                    break
 
     def __handle_client_connection(self, client_sock):
         """
@@ -52,7 +57,7 @@ class Server:
             # TODO: Modify the send to avoid short-writes
             client_sock.send("{}\n".format(msg).encode('utf-8'))
         except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
+            logging.error(f"action: receive_message | result: fail | error: {e}")
         finally:
             client_sock.close()
             logging.info('action: client_sock close | result: success')
