148dae7200576a87acd2c02fb4f9b0b7b86a7c8e
Alexis Herrera <alexysaguilar@gmail.com>
2025-03-27 21:42:29 -0300

    ReadFull to readFull custom

--- client/common/packet.go
+++ client/common/packet.go
@@ -2,7 +2,6 @@ package common
 
 import (
 	"encoding/binary"
-	"io"
 	"net"
 )
 
@@ -23,14 +22,14 @@ func (p *Packet) Write(conn net.Conn) error {
 
 func ReadPacket(conn net.Conn) (*Packet, error) {
 	header := make([]byte, 4)
-	// Usamos io.ReadFull para leer exactamente 4 bytes del header.
-	if _, err := io.ReadFull(conn, header); err != nil {
+	// Usamos readFull para leer exactamente 4 bytes del header.
+	if err := readFull(conn, header); err != nil {
 		return nil, err
 	}
 	totalLength := binary.BigEndian.Uint32(header)
 	data := make([]byte, totalLength)
 	// Leemos exactamente totalLength bytes.
-	if _, err := io.ReadFull(conn, data); err != nil {
+	if err := readFull(conn, data); err != nil {
 		return nil, err
 	}
 	return &Packet{Data: data}, nil
@@ -40,6 +39,19 @@ func writeFull(conn net.Conn, data []byte) error {
 	total := 0
 	for total < len(data) {
 		n, err := conn.Write(data[total:])
+		if err != nil {
+			return err
+		}
+		total += n
+	}
+	return nil
+}
+
+func readFull(conn net.Conn, data []byte) error {
+	// Read all the data from the connection, avoids short reads
+	total := 0
+	for total < len(data) {
+		n, err := conn.Read(data[total:])
 		if err != nil {
 			return err
 		}
